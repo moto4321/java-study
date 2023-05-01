c89a15ffdf85347a71f36e2ee35016e8fe26c1dc
moto4321 <moto432195@gmail.com>
2023-05-01 18:15:27 +0900

    컴포넌트 스캔 대상 수정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", // 이렇게 하면 member만 컴포넌트스캔의 대상이된다.
+        basePackageClasses = AutoAppConfig.class,
+        // 위 두 줄을 지정하지 않으면 hello.core를 뒤진다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
